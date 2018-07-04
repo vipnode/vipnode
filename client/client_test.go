f70209d6a27c50a34b6e36c1b294fedc3876dfa9
Andrey Petrov <andrey.petrov@shazow.net>
2018-07-04 14:51:15 -0400

    client: Basic test

---
+++ client/client_test.go
@@ -0,0 +1,56 @@
+package client
+
+import (
+	"context"
+	"testing"
+
+	"github.com/vipnode/vipnode/ethnode"
+	"github.com/vipnode/vipnode/pool"
+)
+
+type FakeNode struct {
+}
+
+func (n *FakeNode) Enode(ctx context.Context) (string, error) {
+	return "foo", nil
+}
+
+func (n *FakeNode) AddTrustedPeer(ctx context.Context, nodeID string) error {
+	return nil
+}
+
+func (n *FakeNode) RemoveTrustedPeer(ctx context.Context, nodeID string) error {
+	return nil
+}
+
+func (n *FakeNode) ConnectPeer(ctx context.Context, nodeURI string) error {
+	return nil
+}
+
+func (n *FakeNode) DisconnectPeer(ctx context.Context, nodeID string) error {
+	return nil
+}
+
+func (n *FakeNode) Peers(ctx context.Context) ([]ethnode.PeerInfo, error) {
+	return []ethnode.PeerInfo{}, nil
+}
+
+func TestClient(t *testing.T) {
+	p := pool.StaticPool{}
+	client := Client{
+		EthNode: &FakeNode{},
+		Pool:    &p,
+	}
+
+	if err := client.Connect(); err != pool.ErrNoHostNodes {
+		t.Errorf("unexpected no nodes error, got: %q", err)
+	}
+
+	p.Nodes = append(p.Nodes, pool.HostNode{
+		URI: "foo",
+	})
+
+	if err := client.Connect(); err != nil {
+		t.Errorf("unexpected error: %q", err)
+	}
+}
--- pool/staticpool.go
+++ pool/staticpool.go
@@ -6,19 +6,26 @@ import (
 	"time"
 )
 
+// Type assert for Pool implementation.
+var _ Pool = &StaticPool{}
+
 // StaticPool is a dummy implementation of a pool service that always returns from the same set of host nodes.
 type StaticPool struct {
 	Nodes []HostNode
 }
 
-func (s *StaticPool) Connect(ctx context.Context, sig string, nodeID nodeID, timestamp time.Time, kind string) ([]HostNode, error) {
+func (s *StaticPool) Connect(ctx context.Context, sig string, nodeID string, timestamp time.Time, kind string) ([]HostNode, error) {
 	return s.Nodes, nil
 }
 
-func (s *StaticPool) Update(ctx context.Context, sig string, nodeID nodeID, timestamp time.Time, peers string) (*Balance, error) {
+func (s *StaticPool) Disconnect(ctx context.Context, sig string, nodeID string, timestamp time.Time) error {
+	return nil
+}
+
+func (s *StaticPool) Update(ctx context.Context, sig string, nodeID string, timestamp time.Time, peers string) (*Balance, error) {
 	return &Balance{}, nil
 }
 
-func (s *StaticPool) Withdraw(ctx context.Context, sig string, nodeID nodeID, timestamp time.Time) error {
+func (s *StaticPool) Withdraw(ctx context.Context, sig string, nodeID string, timestamp time.Time) error {
 	return errors.New("not implemented")
 }
