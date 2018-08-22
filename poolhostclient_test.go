aa0772312d061a312bfc647703adef679041339e
Andrey Petrov <andrey.petrov@shazow.net>
2018-08-22 15:55:07 -0400

    main: Fix e2e test

--- poolhostclient_test.go
+++ poolhostclient_test.go
@@ -21,19 +21,18 @@ func TestPoolHostClient(t *testing.T) {
 	payout := ""
 
 	p := pool.New()
-	poolserver := &jsonrpc2.Local{}
-	poolserver.Register("vipnode_", p)
+	rpcPool2Host, rpcHost2Pool := jsonrpc2.ServePipe()
+	rpcPool2Host.Register("vipnode_", p)
 
-	nodeID := discv5.PubkeyID(&privkey.PublicKey).String()
-	node := fakenode.Node(nodeID)
-	nodeURI := fmt.Sprintf("enode://%s@127.0.0.1", nodeID)
-	h := host.New(nodeURI, node, "")
-	remotePool := pool.Remote(poolserver, privkey)
-
-	poolserver.Register("vipnode_", h) // Eeeh let's use the same service for the Host.
+	hostNodeID := discv5.PubkeyID(&privkey.PublicKey).String()
+	hostNode := fakenode.Node(hostNodeID)
+	hostNodeURI := fmt.Sprintf("enode://%s@127.0.0.1", hostNodeID)
+	h := host.New(hostNodeURI, hostNode, "")
+	rpcHost2Pool.Register("vipnode_", h)
+	remotePool := pool.Remote(rpcHost2Pool, privkey)
 
 	ctx := context.Background()
-	if err := remotePool.Host(ctx, kind, payout, nodeURI); err != nil {
+	if err := remotePool.Host(ctx, kind, payout, hostNodeURI); err != nil {
 		t.Error(err)
 	}
 
@@ -42,49 +41,29 @@ func TestPoolHostClient(t *testing.T) {
 		t.Error(err)
 	}
 
-	// Try to fake connecting through the RPC
-
-	hosts, err := remotePool.Connect(context.Background(), "geth")
-	if err != nil {
-		t.Error(err)
-	}
-
-	want := fakenode.Calls{fakenode.Call("AddTrustedPeer", nodeID)}
-	if !reflect.DeepEqual(node.Calls, want) {
-		t.Errorf("node.Calls:\n  got %q;\n want %q", node.Calls, want)
-	}
-
-	if len(hosts) != 1 {
-		t.Fatalf("wrong number of hosts: %d", len(hosts))
-	}
-
-	if hosts[0].URI != nodeURI {
-		t.Errorf("wrong host returned: %s", hosts[0].URI)
-	}
-
-	// Actual connecting with a client wrapper
+	rpcPool2Client, rpcClient2Pool := jsonrpc2.ServePipe()
+	rpcPool2Client.Register("vipnode_", p)
 
 	clientPrivkey := keygen.HardcodedKeyIdx(t, 1)
 	clientNodeID := discv5.PubkeyID(&clientPrivkey.PublicKey).String()
 	clientNode := fakenode.Node(clientNodeID)
 	client := client.Client{
 		EthNode: clientNode,
-		Pool:    pool.Remote(poolserver, clientPrivkey),
+		Pool:    pool.Remote(rpcClient2Pool, clientPrivkey),
 	}
 
 	if err := client.Connect(); err != nil {
 		t.Error(err)
 	}
 
-	want = fakenode.Calls{
-		fakenode.Call("AddTrustedPeer", nodeID),
+	want := fakenode.Calls{
 		fakenode.Call("AddTrustedPeer", clientNodeID),
 	}
-	if got := node.Calls; !reflect.DeepEqual(got, want) {
+	if got := hostNode.Calls; !reflect.DeepEqual(got, want) {
 		t.Errorf("node.Calls:\n  got %q;\n want %q", got, want)
 	}
 
-	want = fakenode.Calls{fakenode.Call("ConnectPeer", nodeURI)}
+	want = fakenode.Calls{fakenode.Call("ConnectPeer", hostNodeURI)}
 	if got := clientNode.Calls; !reflect.DeepEqual(got, want) {
 		t.Errorf("node.Calls:\n  got %q;\n want %q", got, want)
 	}
